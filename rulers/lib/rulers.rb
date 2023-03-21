4da179c4dea95f6786ddc831f9cf2ff830842fce
Andrey Sukhikh <andrey.sukhikh@jetruby.com>
2023-03-21 16:32:32 +0300

    Init simple rack based application

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -3,6 +3,9 @@
 require_relative "rulers/version"
 
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, { 'Content-Type' => 'text/html' }, ['Hello, World!']]
+    end
+  end
 end
