1693f59334fc61d5b4599c322454141ed7e5d18d
Andrey Sukhikh <andrey.sukhikh@jetruby.com>
2023-03-21 17:21:38 +0300

    Set up basic routing & controller action

--- best_quotes/config/application.rb
+++ best_quotes/config/application.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-require "rulers"
+require 'rulers'
 
 module BestQuotes
   class Application < Rulers::Application
+    $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'app', 'controllers')
+    require 'quotes_controller'
   end
 end
--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
-require_relative "rulers/version"
+require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, { 'Content-Type' => 'text/html' }, ['Hello, World!']]
+      return [404, { "Content-Type" => "text/html" }, []] if env["PATH_INFO"] == "/favicon.ico"
+
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.public_send(action)
+
+      [200, { "Content-Type" => "text/html" }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
